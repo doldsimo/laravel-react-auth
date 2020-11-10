6f4e858cf79439820d99605b8b95caf7afb76574
doldsimo <simon.dold@hs-furtwangen.de>
2020-11-10 13:25:00 +0100

    Initial commit

---
+++ resources/js/components/Example.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function Example() {
+    return (
+        <div className="container">
+            Hey geht
+        </div>
+    );
+}
+
+export default Example;
+
+// if (document.getElementById('root')) {
+//     ReactDOM.render(<Example />, document.getElementById('root'));
+// }
